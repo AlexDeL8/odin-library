d0d3aa7f4f629592d2f9b21fecd0289ca90e84f7
Alex De Luca <alexnaj88@gmail.com>
2025-07-05 18:49:15 -0400

    feat: dialog form added with styling

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const myLibrary = [];
 
-const newBookBtn = document.getElementById("newBookBtn")
+// ELEMENTS
+const newBookBtn = document.getElementById("newBookBtn");
+const newBookDialog = document.getElementById("newBookDialog");
+const dialogCloseBtn = document.querySelectorAll(".dialogCloseBtn");
+
+// EVENT LISTENERS
 newBookBtn.addEventListener("click", () => {
-    console.log("new button clicked")
+    newBookDialog.showModal()
 })
 
+dialogCloseBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        newBookDialog.close()
+    })
+})
+
+// LOGIC
 function Book(title, author, numOfPages, hasRead = false) {
     if(!new.target) {
         throw new Error("The 'new' operator must be used to call the constructor");
     }
-    this.id = crypto.randomUUID()
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
@@ -17,9 +29,9 @@ function Book(title, author, numOfPages, hasRead = false) {
 }
 
 Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, has ${this.numOfPages} pages. `
+    return `${this.title} by ${this.author}, has ${this.numOfPages} pages. `;
 }
 
 function addBookToLibrary(title, author, numOfPages, hasRead = false) {
-    libraryBooks.push(new Book(title, author, numOfPages, hasRead))
+    libraryBooks.push(new Book(title, author, numOfPages, hasRead));
 }
